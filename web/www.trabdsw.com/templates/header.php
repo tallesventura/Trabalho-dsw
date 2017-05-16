99a31a8b97c76fb3840c6c4a28a2f340c7a7b6c2
Talles Alves <tallesventura@yahoo.com.br>
2017-05-16 16:31:37 -0300

    first commit

---
+++ web/www.trabdsw.com/templates/header.php
@@ -0,0 +1,8 @@
+<html lang="pt-BR">
+<head>
+    <title>Trabalho DSW</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../styles/css/styles.css">
+    <script type="text/javascript" src="../libs/jquery/jquery-3.2.1.min.js"></script>
+</head>
+<body>
---
+++ web/www.trabdsw.com/templates/template_paciente.php
@@ -0,0 +1,38 @@
+
+<?php
+    $pacientes = array(
+                        array("nome"=>"Talles", "foto"=>"http://placehold.it/100x100"),
+                        array("nome"=>"Fulano", "foto"=>"http://placehold.it/100x100"),
+                        array("nome"=>"Ciclano", "foto"=>"http://placehold.it/100x100"));
+?>
+
+<?php foreach($pacientes as $paciente): ?>
+    <!-- Paciente -->
+    <div class="caixa-paciente">
+        <!-- cabeçalho -->
+        <div class="cabecalho">
+            <!-- menu -->
+            <div class="menu-paciente">
+                <!-- nome -->
+                <div>
+                    <p> <?php echo $paciente["nome"]; ?> </p>
+                </div>
+                <!-- ações -->
+                <div>
+                    <button> <img src="http://placehold.it/15x15" alt="botão 1"> </button>
+                    <button> <img src="http://placehold.it/15x15" alt="botão 2"> </button>
+                    <button> <img src="http://placehold.it/15x15" alt="botão 3"> </button>
+                </div>
+            </div>
+            <!-- foto -->
+            <div class="foto-paciente">
+                <img src="<?php echo $paciente['foto'] ?>" alt="Foto do paciente">
+            </div>
+        </div>
+        <!-- corpo -->
+        <div>
+            <p>Dados: <br> </p>
+        </div>
+    </div>
+
+<?php endforeach; ?>
