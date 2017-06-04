14d09cca7312ee09237f590bbef45e20a3253428
Talles Alves <tallesventura@yahoo.com.br>
2017-06-04 20:56:17 -0300

    atualizacao no path do styles.css

--- web/www.trabdsw.com/templates/header.php
+++ web/www.trabdsw.com/templates/header.php
@@ -3,7 +3,7 @@
     <title>Trabalho DSW</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="libs/css/normalize.css">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="estilos/styles.css">
     <script type="text/javascript" src="libs/jquery/jquery-3.2.1.min.js"></script>
 </head>
 <body>
